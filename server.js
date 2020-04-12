eac6e9f1e176ad3d7d102a3f344ff7b3ce812533
Carlos Tenorio Pérez <cartenorioperez@gmail.com>
2020-04-12 15:44:55 +0200

    Add PORT to server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 const dir = __dirname + '/dist/ng-docx-sandbox';
 
 app.use(express.static(dir));
