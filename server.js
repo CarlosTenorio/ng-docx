917b9f625cd7d642fef63cb26738a6654d6ea7e9
Carlos Tenorio Pérez <cartenorioperez@gmail.com>
2020-04-12 15:18:31 +0200

    Add dirname on server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.port || 8080;
-const dir = 'dist/ng-docx-sandbox';
+const dir = __dirname + '/dist/ng-docx-sandbox';
 
 app.use(express.static(dir));
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/' + dir + '/index.html'));
+    res.sendFile(path.join(dir + '/index.html'));
 });
 app.listen(port, function () {
     console.log('App running on port ' + port);
