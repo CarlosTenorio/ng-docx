e32373b6ae5fe832ff4bb4e17b42853d4947efb2
Carlos Tenorio <cartenorioperez@gmail.com>
2020-04-18 10:26:32 +0200

    Add footer (#8)
    * Add custom search engine function
    
    * Add footer

--- projects/ng-docx-sandbox/src/app/modules/home/home.module.ts
+++ projects/ng-docx-sandbox/src/app/modules/home/home.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Route, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 const childRoutes: Route[] = [{ path: '', component: HomeComponent }];
 
 @NgModule({
-    declarations: [HomeComponent],
+    declarations: [HomeComponent, FooterComponent],
     imports: [CommonModule, RouterModule.forChild(childRoutes)]
 })
 export class HomeModule {}
