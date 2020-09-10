cf83551d62211316229e56e6d172811b9def7e3d
Carlos Tenorio <cartenorioperez@gmail.com>
2020-09-10 10:18:21 +0200

    Explicit imports on docs module (#25)

--- projects/ng-docx/src/lib/modules/docs/docs.module.ts
+++ projects/ng-docx/src/lib/modules/docs/docs.module.ts
@@ -10,19 +10,17 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import {
-    NavigationTreeComponent,
-    NavigationMenuComponent,
-    SearchComponent,
-    VersioningComponent,
-    EditButtonComponent,
-    SnackBarCopyComponent
-} from './components';
-import { ClickOutsideDirective } from './directives';
-import { NgDocxComponent } from './containers';
-import { ConfigInterface } from './models';
-import { MarkdownModule } from 'ngx-markdown';
+import { MarkdownModule } from 'ngx-markdown/';
 import { FileSystemService } from './services/file-system/file-system.service';
+import { NavigationTreeComponent } from './components/navigation-tree/navigation-tree.component';
+import { NavigationMenuComponent } from './components/navigation-menu/navigation-menu.component';
+import { SearchComponent } from './components/search/search.component';
+import { VersioningComponent } from './components/versioning/versioning.component';
+import { EditButtonComponent } from './components/edit-button/edit-button.component';
+import { SnackBarCopyComponent } from './components/snack-bar-copy/snack-bar-copy.component';
+import { NgDocxComponent } from './containers/docs/docs.component';
+import { ClickOutsideDirective } from './directives/click-outside.directive';
+import { ConfigInterface } from './models/configInterface';
 
 const childRoutes: Route[] = [{ path: '', component: NgDocxComponent }];
 
